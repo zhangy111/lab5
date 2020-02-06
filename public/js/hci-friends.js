d7d5274dcf1e8473ba13b655241d09ea42c6449c
Henry Zhang <yuz087@ucsd.edu>
2020-02-06 11:12:36 -0800

    finished tasks

--- public/js/hci-friends.js
+++ public/js/hci-friends.js
@@ -5,11 +5,22 @@ $(document).ready(function() {
 	initializePage();
 })
 
+
 /*
  * Function that is called when the document is ready.
  */
 function initializePage() {
 	console.log("Javascript connected!");
+	$(".name a").click(listenerFunction);
+}
+
+function listenerFunction(e)
+{
+	e.preventDefault();
+	var name = $(this).text();
+	var newName = anagrammedName(name);
+	console.log(newName);
+	$(this).text(newName);
 }
 
 function anagrammedName(name) {
--- routes/add.js
+++ routes/add.js
@@ -1,5 +1,8 @@
 var data = require("../data.json");
 
-exports.addFriend = function(request, response) {    
+exports.addFriend = function(request, response) {   
 	// Your code goes here
- }
+	data.friends.push({"name": request.query.name, "description": request.query.description, "imageURL": "http://lorempixel.com/400/400/people"});
+	console.log(data.friends);
+	response.render("index", data);
+}
